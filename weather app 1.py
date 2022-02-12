fa36b27bd973ea91db97b00309e611f1bca38c82
Pushkar299 <98707979+Pushkar299@users.noreply.github.com>
2022-02-13 01:02:13 +0530

    Add files via upload

---
+++ weather app 1.py
@@ -0,0 +1,74 @@
+import tkinter as tk
+import requests
+from PIL import Image,ImageTk  #pip install pillow
+
+
+root=tk.Tk()
+
+root.title("Weather App")
+root.geometry("600x500")
+
+#key: 134a0fa436d74ecc68bf8d7420b1bd4d
+#API url: api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+def format_response(weather):
+    try:
+        city=weather['name']
+        condition=weather['weather'][0]['description']
+        temp=weather['main']['temp']
+        final_str='City:%s\nCondition:%s\nTemperature:%s'%(city,condition,temp)
+    except:
+        final_str='There was a problem retrieving that information'
+    return final_str
+
+
+
+def get_weather(city):
+    weather_key='134a0fa436d74ecc68bf8d7420b1bd4d'
+    url='https://api.openweathermap.org/data/2.5/weather'
+    params={'appid':weather_key,'q':city,'units':'imperial'}
+    response=requests.get(url,params)
+    #print(response.json())
+    weather=response.json()
+
+    print(weather['name'])
+    print(weather['weather'][0]['description'])
+    print(weather['main']['temp'])
+
+    result['text']=format_response(weather)
+    
+
+
+img=Image.open('./bg.png')
+img=img.resize((600,500),Image.ANTIALIAS)
+img_photo=ImageTk.PhotoImage(img)
+
+bg_lbl=tk.Label(root,image=img_photo)
+bg_lbl.place(x=0,y=0,width=600,height=500)
+
+heading_title=tk.Label(bg_lbl,text='Earth including over 200,000 cities!',fg='red',bg='sky blue',font=('times now roman',18,'bold'))
+heading_title.place(x=80,y=18)
+
+
+frame_one=tk.Frame(bg_lbl,bg="#42c2f4",bd=5)
+frame_one.place(x=80,y=60,width=450,height=50)
+
+txt_box=tk.Entry(frame_one,font=('times new roman',25),width=17)
+txt_box.grid(row=0,column=0,sticky='W')
+
+btn=tk.Button(frame_one,text='get weather',fg='green',font=('times new roman',16,'bold'),command=lambda: get_weather(txt_box.get()))
+btn.grid(row=0,column=1,padx=10)
+
+
+frame_two=tk.Frame(bg_lbl,bg="white",bd=5)
+frame_two.place(x=80,y=130,width=450,height=300)
+
+result=tk.Label(frame_two,font=40,bg="white",justify='left',anchor='nw')
+result.place(relwidth=1,relheight=1)
+
+
+
+
+
+
+root.mainloop()
+
